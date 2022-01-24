d9ac2a9b5732fb85b2b1d65bf2e5f01dc910e24a
yuuki1036 <yuuki1036@gmail.com>
2022-01-24 20:08:44 +0900

    add avatar

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -24,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        $url->forceScheme('https');
+        if (config('app.env') === 'production') {
+            $url->forceScheme('https');
+        }
     }
 }
--- backend/resources/js/components/Tweet.js
+++ backend/resources/js/components/Tweet.js
@@ -2,29 +2,39 @@ import React from "react";
 import { format } from "date-fns";
 import { FaRetweet, FaRegComment } from "react-icons/fa";
 import { FcLikePlaceholder } from "react-icons/fc";
+import Avatar from "react-avatar";
+import MyAvatar from "./Avatar";
 
 export default function Tweet({ tweet }) {
     return (
-        <div className="p-2">
-            <p>
-                <span className="text-lg font-bold mr-4">{tweet.userName}</span>
-                <span className="text-sm text-slate-600">
-                    {format(new Date(tweet.created_at), "MMM d, yyyy")}
-                </span>
-            </p>
-            <p>{tweet.content}</p>
-            <div className="flex">
-                <div className="flex items-center mr-4">
-                    <div className="mr-2">{<FaRegComment />}</div>
-                    <div>{tweet.replay}</div>
-                </div>
-                <div className="flex items-center mr-4">
-                    <div className="mr-2">{<FaRetweet />}</div>
-                    <div>{tweet.retweet}</div>
-                </div>
-                <div className="flex items-center">
-                    <div className="mr-2">{<FcLikePlaceholder />}</div>
-                    <div>{tweet.likes}</div>
+        <div className="flex p-2">
+            <div className="mt-2 mr-4">
+                <MyAvatar name={tweet.userName} />
+            </div>
+            <div>
+                <p>
+                    <span></span>
+                    <span className="text-lg font-bold mr-4">
+                        {tweet.userName}
+                    </span>
+                    <span className="text-sm text-slate-600">
+                        {format(new Date(tweet.created_at), "MMM d, yyyy")}
+                    </span>
+                </p>
+                <p>{tweet.content}</p>
+                <div className="flex">
+                    <div className="flex items-center mr-5">
+                        <div className="mr-2">{<FaRegComment />}</div>
+                        <div>{tweet.replay}</div>
+                    </div>
+                    <div className="flex items-center mr-5">
+                        <div className="mr-2">{<FaRetweet />}</div>
+                        <div>{tweet.retweet}</div>
+                    </div>
+                    <div className="flex items-center">
+                        <div className="mr-2">{<FcLikePlaceholder />}</div>
+                        <div>{tweet.likes}</div>
+                    </div>
                 </div>
             </div>
         </div>
